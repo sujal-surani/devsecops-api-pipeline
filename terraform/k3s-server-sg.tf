48a84b7980fd01a0e9d9e8c72c42a797ea4680eb
sujal-hp <suranisujal19@gmail.com>
2026-09-08 13:53:24 +0530

    feat: provision k3s infrastructure

--- terraform/k3s-server-sg.tf
+++ terraform/k3s-server-sg.tf
@@ -6,20 +6,20 @@ resource "aws_security_group" "k3s-sg" {
     from_port = 6443
     to_port   = 6443
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress{
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   egress{
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
